9386e83f649a0b96b5ed135da996ce9ad9760ea7
MichaelAdebayo <mikea14071@gmail.com>
2024-01-30 22:04:08 -0500

    Adding changes

--- Java_code/ExpenseTrackerUi.java
+++ Java_code/ExpenseTrackerUi.java
@@ -1,4 +1,8 @@
+import org.w3c.dom.events.Event;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -6,8 +10,6 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 public class ExpenseTrackerUi extends Application {
-
-    
  public static void main(String[] args) {
         launch(args);
     }
@@ -24,14 +26,35 @@ public void start(Stage primaryStage) throws Exception {
     Label userNameLabel = new Label("User Name:");
     TextField userNameTextField = new TextField();
 
-    Button addUserButton = new Button("Sign Up");
+    Button signUp = new Button("Sign Up");
     Button signIn = new Button("Sign In");
 
-    StackPane stackPane = new StackPane();
-    stackPane.getChildren().add(addUserButton);
+    //StackPane stackPane = new StackPane();
+   //stackPane.getChildren().add(signUp);
+    
+      GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+        gridPane.setVgap(8);
+        gridPane.setHgap(10);
+
+        // Add components to the layout
+        gridPane.add(userIdLabel, 0, 0);
+        gridPane.add(userIdTextField, 1, 0);
+        gridPane.add(userNameLabel, 0, 1);
+        gridPane.add(userNameTextField, 1, 1);
+        gridPane.add(signUp, 1, 2);
+        gridPane.add(signIn, 0, 2);
+
+signUp.setOnAction( e -> {
+    System.out.println("Sign Up Clicked");
+    System.out.println("User ID: " + userIdTextField.getText());
+    System.out.println("User Name: " + userNameTextField.getText());
+});{
+
+}
     
 
-    Scene scene = new Scene(stackPane, 300, 250);
+    Scene scene = new Scene(gridPane, 300, 250);
     primaryStage.setScene(scene);
     primaryStage.show();
 
@@ -39,5 +62,7 @@ public void start(Stage primaryStage) throws Exception {
 }
 
 
+
+   
 
 }
